4423de4ebb2b53ebb33406f1220e4c7e48832dd0
Harsh91221 <harshch91221@gmail.com>
2023-01-19 01:19:24 +0530

    Queue

---
+++ QUEUE/InterleaveFirstHalf.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <stack>
+void interLeaveQueue(queue<int> &q)
+{
+    queue<int> newq;
+    int half = q.size() / 2;
+    for (int i = 0; i < half; i++)
+    {
+        int val = q.front();
+        q.pop();
+        newq.push(val);
+    }
+    while (!newq.empty())
+    {
+        int val = newq.front();
+        newq.pop();
+        q.push(val);
+        val = q.front();
+        q.pop();
+        q.push(val);
+    }
+}
