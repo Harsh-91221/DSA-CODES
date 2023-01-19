557192c1fb6b166fe520e96d068efd2a6b302088
Harsh91221 <harshch91221@gmail.com>
2023-01-20 00:59:40 +0530

    Tree

---
+++ TREES/BinaryTree.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+    node(int d)
+    {
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+node *buildtree(node *root)
+{
+    int data;
+    cout << "ENTER THE DATA: " << endl;
+    cin >> data;
+    root = new node(data);
+    if (data == -1)
+    {
+        return NULL;
+    }
+    cout << "ENTER DATA FOR INSERTING IN LEFT SIDE OF " << data << endl;
+    root->left = buildtree(root->left);
+    cout << "ENTER DATA FOR INSERTING IN RIGHT SIDE OF " << data << endl;
+    root->right = buildtree(root->right);
+    return root;
+}
+void levelOrderTraversal(node *root)
+{
+    queue<node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty())
+    {
+        node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+int main()
+{
+    node *root = NULL;
+    root = buildtree(root);
+    levelOrderTraversal(root);
+}
