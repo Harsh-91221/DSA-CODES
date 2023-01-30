864b0568549353d82b733e0a23125de72127e5c7
Harsh91221 <harshch91221@gmail.com>
2023-01-30 22:44:47 +0530

    Check

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -1,58 +1,10 @@
 #include <iostream>
 using namespace std;
-class TreeNode
-{
-public:
-    int data;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int d)
-    {
-        this->data = d;
-        this->left = NULL;
-        this->right = NULL;
-    }
-};
-TreeNode *buildtree(TreeNode *root)
-{
-    int data;
-    cout << "ENTER THE DATA: " << endl;
-    cin >> data;
-    root = new TreeNode(data);
-    if (data == -1)
-    {
-        return NULL;
-    }
-    cout << "ENTER DATA FOR INSERTING IN LEFT SIDE OF " << data << endl;
-    root->left = buildtree(root->left);
-    cout << "ENTER DATA FOR INSERTING IN RIGHT SIDE OF " << data << endl;
-    root->right = buildtree(root->right);
-    return root;
-}
-void solve(Node *root, vector<int> &ans, int level)
-{
-    if (root == NULL)
-    {
-        return;
-    }
-    if (level == ans.size())
-    {
-        ans.push_back(root->data);
-    }
-    solve(root->left, ans, level + 1);
-    solve(root->right, ans, level + 1);
-}
-
-vector<int> leftView(Node *root)
-{
-    vector<int> ans;
-    solve(root, ans, 0);
-    return ans;
-}
 int main()
 {
-    TreeNode *root = NULL;
-    root = buildtree(root);
-    cout << "LEFT VIEW IS : ";
-    leftView(root);
+    int n;
+    if (cin >> n)
+    {
+        cout << "HARSHIEE";
+    }
 }
