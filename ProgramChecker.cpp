51a85615e57ddfda34f9493246974bff3b3b498d
Harsh91221 <harshch91221@gmail.com>
2023-02-04 23:29:03 +0530

    C++

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -3,8 +3,18 @@ using namespace std;
 int main()
 {
     int n;
-    if (cin >> n)
+    cin >> n;
+    for (int i = 1; i <= n; i++)
     {
-        cout << "HARSHIEE";
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+        int j = i;
+        for (;j > 0; j--)
+        {
+            cout << j - 1;
+        }
+        cout << endl;
     }
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+for (int i = 1; i <= m; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for (int k = 1; k <= m - i + 1; k++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
