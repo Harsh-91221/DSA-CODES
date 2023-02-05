8969e62af4225247f49d346ed47383053a095dc1
Harsh91221 <harshch91221@gmail.com>
2023-02-06 00:00:00 +0530

    C++

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -2,19 +2,6 @@
 using namespace std;
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= i; j++)
-        {
-            cout << j;
-        }
-        int j = i;
-        for (;j > 0; j--)
-        {
-            cout << j - 1;
-        }
-        cout << endl;
-    }
+    int n = 5;
+    cout << (++n) * (++n);
 }
