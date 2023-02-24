92a092c870072f82517c2c238448bbc0e2df47a1
Harsh91221 <harshch91221@gmail.com>
2023-02-25 01:28:30 +0530

    TEMP

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -3,43 +3,13 @@
 using namespace std;
 int main()
 {
-    int n, sum;
-    cout << "ENTER THE NUMBER OF ELEMENTS " << endl;
-    cin >> n;
-    vector<int> a(n);
-    vector<int> b(n);
-    vector<int> c(n);
-    cout << "ENTER FIRST ARRAY " << endl;
-    for (int i = 0; i < n; i++)
+    vector<vector<int>> transpose(5, vector<int>(5, -8));
+    for (int i = 0; i < transpose.size(); i++)
     {
-        cin >> a[i];
-    }
-    cout << "ENTER SECOND ARRAY " << endl;
-    for (int j = 0; j < n; j++)
-    {
-        cin >> b[j];
-    }
-    cout << "ENTER THIRD ARRAY " << endl;
-    for (int k = 0; k < n; k++)
-    {
-        cin >> c[k];
-    }
-    cout << "ENTER THE SUM OF PAIR " << endl;
-    cin >> sum;
-    for (int i = 0; i < n; i++)
-    {
-        int element1 = a[i];
-        for (int j = i + 1; j < n; j++)
+        for (int j = 0; j < transpose[0].size(); j++)
         {
-            int element2 = a[j];
-            for (int k = j + 1; k < n; k++)
-            {
-                int element3 = a[k];
-                if (element1 + element2 + element3 == sum)
-                {
-                    cout << "PAIR FOUND "<< "(" << element1 << "," << element2 << "," << element3 << ")" << endl;
-                }
-            }
+            cout << transpose[i][j] << " ";
         }
+        cout << endl;
     }
 }
