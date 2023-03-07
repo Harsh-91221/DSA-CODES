3bd0ad80c551f4d690011c037f3b3a7000a1e0a0
Harsh91221 <harshch91221@gmail.com>
2023-03-07 23:56:19 +0530

    DSA

--- C PROGRAMMING/sumofeight.c
+++ C PROGRAMMING/sumofeight.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int n,i,sum=0,t;
+    int n, i, sum = 0, t;
     printf("Enter the number for series ");
-    scanf("%d",&t);
+    scanf("%d", &t);
     printf("Enter the limit of sum ");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
     {
-         sum=sum+ i*t;
-    
+        sum = sum + i * t;
     }
-    printf("Sum = %d",sum);
+    printf("Sum = %d", sum);
 }
--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -3,13 +3,17 @@
 using namespace std;
 int main()
 {
-    vector<vector<int>> transpose(5, vector<int>(5, -8));
-    for (int i = 0; i < transpose.size(); i++)
+    int i = 2;
+    int n;
+    cin >> n;
+    while (i < n)
     {
-        for (int j = 0; j < transpose[0].size(); j++)
+        if (n % i == 0)
         {
-            cout << transpose[i][j] << " ";
+            cout << "NOT PRIME" << endl;
+            break;
         }
-        cout << endl;
+        i++;
     }
+    cout << "PRIME" << endl;
 }
