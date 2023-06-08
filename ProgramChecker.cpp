3013a5929fff5b5b16605c59d2f2f8c077db5d82
Harsh91221 <harshch91221@gmail.com>
2023-06-08 12:03:01 +0530

    PRACTICE

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 using namespace std;
+bool check(int arr[])
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            return false;
+        }
+    }
+    return true;
+}
 int main()
 {
-    int n;
-    int count = 1;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int arr[5] = {2, 90, 6, 9, 10};
+    bool ans = check(arr);
+    if (ans)
     {
-        for (int j = 1; j < i + 1; j++)
-        {
-            cout << count << " ";
-            count++;
-        }
-        cout << endl;
+        cout << "SORTED" << endl;
+    }
+    else
+    {
+        cout << "NOT SORTED" << endl;
     }
 }
