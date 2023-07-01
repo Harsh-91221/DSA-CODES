f9f65500157a646436c4255dc16b7bef0ad836ac
Harsh91221 <harshch91221@gmail.com>
2023-07-01 22:47:26 +0530

    Program

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
-#include <vector>
-#include <limits.h>
 using namespace std;
-int solve(vector<int> &arr, int target)
-{
-    if (target == 0)
-    {
-        return 0;
-    }
-    if (target < 0)
-    {
-        return INT_MAX;
-    }
-    int mini = INT_MAX;
-    for (int i = 0; i < arr.size(); i++)
-    {
-        int ans = solve(arr, target - arr[i]);
-        if (ans != INT_MAX)
-        {
-            mini = min(mini, ans + 1);
-        }
-    }
-    return mini;
-}
 int main()
 {
-    vector<int> arr{1, 2, 5};
-    int target = 10;
-    cout << solve(arr, target);
+    string str1 = "";
+    string str2 = "";
+    cin >> str1;
+    cin >> str2;
+    for (int i = 0; i < str1.length(); i++)
+    {
+        if (str1[i] < 92)
+        {
+            str1[i] = str1[i] + 32;
+        }
+        if (str2[i] < 92)
+        {
+            str2[i] = str2[i] + 32;
+        }
+    }
+    if (str1 < str2)
+    {
+        cout << "-1";
+    }
+    if (str1 > str2)
+    {
+        cout << "1";
+    }
+    if (str1 == str2)
+    {
+        cout << "0";
+    }
 }
