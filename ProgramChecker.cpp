15daa9cba56e4c3f4500058fb55f5f7b3a5d2f17
Harsh91221 <harshch91221@gmail.com>
2023-07-13 00:06:53 +0530

    CODE

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -1,33 +1,13 @@
-// Input
-// 40047
-
-// Output
-// NO
-
-/*Explanation-> Petya loves lucky numbers.
-We all know that lucky numbers are the positive integers whose decimal representations contain only the lucky digits 4 and 7.
-For example, numbers 47, 744, 4 are lucky and 5, 17, 467 are not.*/
-
 #include <iostream>
 using namespace std;
 int main()
 {
-    long long int n, count = 0;
+    int n;
     cin >> n;
-    while (n != 0)
+    while (n--)
     {
-        if (n % 10 == 4 || n % 10 == 7)
-        {
-            count++;
-        }
-        n = n / 10;
-    }
-    if (count == 4 || count == 7)
-    {
-        cout << "YES";
-    }
-    else
-    {
-        cout << "NO";
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
     }
 }
