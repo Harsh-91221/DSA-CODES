11b37f5ecd70e79e7adab870881fc071418ceb80
Harsh91221 <harshch91221@gmail.com>
2023-07-13 13:13:54 +0530

    CODE

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 int main()
 {
-    int n;
+    int a[100], n, count = 0, sum1 = 0, sum2 = 0;
     cin >> n;
-    while (n--)
+    for (int i = 0; i < n; i++)
     {
-        int a, b;
-        cin >> a >> b;
-        cout << a + b << endl;
+        cin >> a[i];
+        sum1 += a[i];
     }
+    int half = sum1 / 2;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+    for (int i = n - 1; i >= 0; i--)
+    {
+        sum2 += a[i];
+        count++;
+        if (sum2 > half)
+        {
+            break;
+        }
+    }
+    cout << count;
+    return 0;
 }
