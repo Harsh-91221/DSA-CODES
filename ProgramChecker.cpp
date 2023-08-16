b1fff35997ae5af0c9203835aa88614a8d6348c5
Harsh91221 <harshch91221@gmail.com>
2023-08-16 16:19:37 +0530

    CODE

--- ProgramChecker.cpp
+++ ProgramChecker.cpp
@@ -2,26 +2,7 @@
 using namespace std;
 int main()
 {
-    int n;
+    long int n;
     cin >> n;
-    if (n == 1)
-    {
-        cout << "NOT PRIME";
-    }
-    else if (n > 1)
-    {
-        for (int i = 2; i < n / 2 + 1; i++)
-        {
-            if (n % i == 0)
-            {
-                cout << "NOT PRIME";
-                break;
-            }
-        }
-        cout << "PRIME";
-    }
-    else
-    {
-        cout << "NOT PRIME";
-    }
+    cout << n / 13;
 }
