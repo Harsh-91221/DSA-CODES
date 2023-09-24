a2db503041bfa9405ba4d06d36d9f19baeba6b30
Harsh91221 <harshch91221@gmail.com>
2023-09-24 17:02:05 +0530

    CODE

---
+++ STACKS/PrevSmallerElement.cpp
@@ -0,0 +1,20 @@
+#include <stack>
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    for (int i = 0; i < n; i++)
+    {
+        int curr = arr[i];
+        while (s.top() >= curr)
+        {
+            s.pop();
+        }
+        // CHOTA ELEMENT MIL GAYA STORE IN ANS
+        ans[i] = s.top();
+        // PUSH KARDO CURR KO
+        s.push(curr);
+    }
+    return ans;
+}
